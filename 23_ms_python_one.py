f0324789e17fd14b73d9cb96a910eabf360101c6
Kenn Gorman <kenngorman@users.noreply.github.com>
2023-03-23 14:12:06 -0500

    Add files via upload

---
+++ 23_ms_python_one.py
@@ -0,0 +1,20 @@
+# Joe Student
+# Python One
+# Spring 2023
+
+'''
+This is my first Python project
+'''
+print('Joe Student')
+print(16)
+print(False)
+print(100.100)
+name = 'Joe Student'
+age = 58
+is_in_school = False
+desired_gpa = 3.8
+print(name)
+print('My name is', name, 'and I am', age, 'years old.')
+print('I want to finish school with a',
+      desired_gpa,
+      'GPA.')
